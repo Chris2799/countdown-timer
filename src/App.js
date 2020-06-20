a4d88611977781c50950b60ec22eb865b5a95528
Christos Deretzis <deretzischristos@gmail.com>
2020-06-20 21:33:59 +0300

    Initial commit of countdown timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Countdown from './Countdown';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h3 className="title">New Year's Eve is coming soon (Midnight of 30th to 31st Dec, UTC time):</h3>
+          <Countdown date={`${year}-12-30T00:00:00`} />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Countdown.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Countdown extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+    }
+
+    componentDidMount(){
+        //update every second
+        this.interval = setInterval(() => {
+            const date = this.calculateCountDown(this.props.date);
+            date ? this.setState(date) : this.stop();
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        this.stop();
+    }
+
+    calculateCountDown(endDate){
+        // get the difference in seconds in seconds 
+        let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+
+        // if we want a date before our current date, return false
+        if(diff <= 0) return false;
+
+        let timeLeft = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+
+        //Turn the seconds into our parameters of our timeLeft object
+        if(diff > 86400){ // 24 * 60 * 60
+            timeLeft.days = Math.floor(diff / 86400);
+            diff -= timeLeft.days * 86400;
+        }
+        if(diff > 3600){ // 60 * 60
+            timeLeft.hours = Math.floor(diff / 3600);
+            diff -= timeLeft.hours * 3600;
+        }
+        if(diff > 60){
+            timeLeft.minutes = Math.floor(diff / 60);
+            diff -= timeLeft.minutes * 60;
+        }
+        timeLeft.seconds = diff;
+
+        return timeLeft;
+    }
+
+    stop() {
+        clearInterval(this.interval);
+    }
+
+    addLeadingZeros(value){
+        value = String(value);
+        while(value.length < 2){
+            value = '0' + value;
+        }
+        return value;
+    }
+
+    render(){
+        const timeLeft = this.state;
+
+        return(
+            <div className="Countdown">
+                <span className="Countdown-col">
+                    <span className="Countdown-col-element">
+                        <strong>{this.addLeadingZeros(timeLeft.days)}</strong>
+                        <span>Days</span>
+                    </span>
+                </span>
+
+                <span className="Countdown-col">
+                    <span className="Countdown-col-element">
+                        <strong>{this.addLeadingZeros(timeLeft.hours)}</strong>
+                        <span>Hours</span>
+                    </span>
+                </span>
+
+                <span className="Countdown-col">
+                    <span className="Countdown-col-element">
+                        <strong>{this.addLeadingZeros(timeLeft.minutes)}</strong>
+                        <span>Minutes</span>
+                    </span>
+                </span>
+
+                <span className="Countdown-col">
+                    <span className="Countdown-col-element">
+                        <strong>{this.addLeadingZeros(timeLeft.seconds)}</strong>
+                        <span>Seconds</span>
+                    </span>
+                </span>
+            </div>
+        );
+    }
+}
+
+Countdown.propTypes = {
+    date: PropTypes.string.isRequired
+};
+
+Countdown.defaultProps = {
+    date: new Date()
+};
+
+export default Countdown;
